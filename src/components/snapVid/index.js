854484f95a4215bd396dc705be8c357be508d357
Daniel Lopez <daniel@bldrscove.com>
2021-09-23 01:32:03 -0400

    Added bg video section on home page.

--- src/components/snapVid/index.js
+++ src/components/snapVid/index.js
@@ -3,24 +3,36 @@ import { theme } from "twin.macro";
 
 import Button from "../lib/Button";
 import PageLayoutWrapper from "../layoutWrappers/PageLayoutWrapper";
+import backgroundVid from "../../videos/fixing-wall-bg-video.mp4";
 
 export default function Snapavid() {
   return (
     <PageLayoutWrapper tw='md:w-full md:max-w-full 2xl:(w-11/12 max-w-screen-2xl)'>
       <div tw='grid grid-cols-1 gap-0 md:grid-cols-2 md:height[26rem]'>
-        <div className='snap-video-wrapper' tw='hidden md:block'>
+        <div className='video-wrapper' tw='hidden md:block'>
           <div
             className='top-section'
-            tw='h-full bg-mildgray flex flex-wrap justify-center items-center p-8'
+            tw='relative h-full flex bg-ltgray flex-wrap justify-center items-center overflow-hidden 2xl:(rounded-tl-md rounded-bl-md)'
           >
-            <h2 tw='mb-4 text-center text-white w-full'>This is on the LEFT</h2>
+            <div
+              className='vid-color-overlay'
+              tw='absolute z-10 h-full w-full opacity-75 backgroundColor[#d4cdc8] mix-blend-hard-light'
+            />
+            <video
+              autoPlay
+              loop
+              tw='w-full h-full opacity-90 object-cover object-center hidden md:block md:height[29rem]'
+            >
+              <source src={backgroundVid} type='video/mp4' />
+              <p>Your browser doesn't support HTML5 video.</p>
+            </video>
           </div>
         </div>
         <PageLayoutWrapper tw='md:w-full md:max-w-full'>
           <div className='snap-video-wrapper' tw='h-full'>
             <div
               className='top-section'
-              tw='bg-mildgray rounded-t-md flex flex-wrap justify-center items-center p-6 sm:p-8 md:rounded-none'
+              tw='backgroundColor[#3c3b3b] rounded-t-md flex flex-wrap justify-center items-center p-6 sm:p-8 md:rounded-none 2xl:rounded-tr-md'
             >
               <h2 tw='mb-4 text-center text-white w-full'>Snap a Video</h2>
               <p tw='text-white max-w-sm text-center leading-6'>
@@ -30,7 +42,7 @@ export default function Snapavid() {
             </div>
             <div
               className='btm-section'
-              tw='height[calc(26rem*.75)] bg-softGreen rounded-b-md flex flex-wrap justify-center items-center md:rounded-none p-6 sm:p-8'
+              tw='height[calc(26rem*.75)] bg-softGreen rounded-b-md flex flex-wrap justify-center items-center md:rounded-none p-6 sm:p-8 2xl:rounded-br-md'
             >
               <div tw='h-52 flex flex-wrap justify-center items-center'>
                 <p tw='text-white max-w-sm text-center leading-6'>
