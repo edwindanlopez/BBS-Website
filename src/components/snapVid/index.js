4dba7cf9ecc753ccb67fece22c28dc95adfd5808
Daniel Lopez <daniel@bldrscove.com>
2021-10-01 03:03:31 -0400

    Temporarily linking btn to contact page.

--- src/components/snapVid/index.js
+++ src/components/snapVid/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { theme } from "twin.macro";
 import { StaticImage } from "gatsby-plugin-image";
+import { Link } from "gatsby";
 
 import Button from "../lib/Button";
 import PageLayoutWrapper from "../layoutWrappers/PageLayoutWrapper";
@@ -52,39 +53,41 @@ export default function Snapavid() {
                   Shoot us a video showing us the space and we’ll help identify
                   a couple of possibilities to help get you started.
                 </p>
-                <Button
-                  id='hero-btn'
-                  type='button'
-                  variant='secondary'
-                  tw='w-3/4'
-                >
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    width='31.6'
-                    height='17'
+                <Link to={`/contact/`}>
+                  <Button
+                    id='hero-btn'
+                    type='button'
+                    variant='secondary'
+                    tw='w-3/4'
                   >
-                    <g
-                      data-name='Group 5'
-                      transform='translate(-3 -16)'
-                      fill={theme`colors.mildGray`}
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      width='31.6'
+                      height='17'
                     >
-                      <rect
-                        data-name='Rectangle 34'
-                        width='20'
-                        height='17'
-                        rx='3'
-                        transform='translate(3 16)'
-                      />
-                      <path
-                        data-name='Path 24'
-                        d='M33.507 17.093l-7.65 4.371v6.557l7.65 4.372H34.6v-15.3z'
-                      />
-                    </g>
-                  </svg>
-                  <span tw='inline-block pl-3 font-sans font-semibold'>
-                    Snap Vid
-                  </span>
-                </Button>
+                      <g
+                        data-name='Group 5'
+                        transform='translate(-3 -16)'
+                        fill={theme`colors.mildGray`}
+                      >
+                        <rect
+                          data-name='Rectangle 34'
+                          width='20'
+                          height='17'
+                          rx='3'
+                          transform='translate(3 16)'
+                        />
+                        <path
+                          data-name='Path 24'
+                          d='M33.507 17.093l-7.65 4.371v6.557l7.65 4.372H34.6v-15.3z'
+                        />
+                      </g>
+                    </svg>
+                    <span tw='inline-block pl-3 font-sans font-semibold'>
+                      Snap Vid
+                    </span>
+                  </Button>
+                </Link>
               </div>
             </div>
           </div>
