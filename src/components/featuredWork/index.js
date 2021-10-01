e58439b8372d7e697c68bb5ac1ba0606b59f44e0
Daniel Lopez <daniel@bldrscove.com>
2021-10-01 03:07:03 -0400

    git config core.ignorecase false

---
+++ src/components/featuredWork/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import "twin.macro";
+
+import FeatWorkTiles from "./FeatWorkTiles";
+import PageLayoutWrapper from "../layoutWrappers/PageLayoutWrapper";
+
+const FeaturedWork = () => {
+  const data = useStaticQuery(query);
+  return (
+    <PageLayoutWrapper>
+      <h2 tw='mb-4'>Featured Works</h2>
+      <FeatWorkTiles data={data} />
+    </PageLayoutWrapper>
+  );
+};
+
+export default FeaturedWork;
+
+const query = graphql`
+  query queryFeaturedWork {
+    allMdx(filter: { frontmatter: { featured_type: { eq: "Primary" } } }) {
+      nodes {
+        id
+        frontmatter {
+          hero_image_alt
+          title
+          hero_image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+        slug
+      }
+    }
+  }
+`;
