9fec5391d415b369230294fd0d7df124291fb6d2
Daniel Lopez <daniel@bldrscove.com>
2022-01-26 18:02:50 -0500

    Update readme.

--- functions/contactFormSubmission.js
+++ functions/contactFormSubmission.js
@@ -6,7 +6,7 @@ sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 const CORS_HEADERS = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Headers":
-    "Origin, X-Requested-With, Content-Type, Accept",
+  "Origin, X-Requested-With, Content-Type, Accept",
   "Access-Control-Allow-Methods": "POST, OPTIONS",
 };
 
